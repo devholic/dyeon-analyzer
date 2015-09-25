4980328c0d89be286cae64873a3b7b8fb9ddb7f4
devholic : MBPr (Mac) <devholic@plusquare.com>
2015-09-26 04:55:57 +0900

    add checking arguments / redis support / date checking

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/fatih/color"
 	"github.com/garyburd/redigo/redis"
@@ -23,6 +22,10 @@ var username, password string
 
 // target information
 var year, month, date string
+
+// parse
+var nextPage bool = true
+var enterLoop bool = false
 
 var httpClient = initHttpClient()
 var redisClient = connectRedis()
@@ -57,10 +60,8 @@ func login() bool {
 	}
 }
 
-func postCheck() {
-}
-
 func getPostList(idx int) {
+	color.Yellow("[!] Parse page #", idx)
 	resp, _ := httpClient.Get(DYEON_BOARD_URL + "?page=" + strconv.Itoa(idx))
 	defer resp.Body.Close()
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
@@ -68,18 +69,49 @@ func getPostList(idx int) {
 		log.Fatal(err)
 	}
 	t := doc.Find("tbody").First()
-	t.Find("tr").Not(".ad").Not(".notice").Each(func(i int, s *goquery.Selection) {
-		link, exists := s.Find(".subject").Find("a").Attr("href")
-		if exists {
-			if strings.HasPrefix(link, "http://") {
-        fmt.Println(link)
+	t.Find("tr").Not(".ad").Not(".notice").EachWithBreak(func(_ int, s *goquery.Selection) bool {
+		// date extract
+		d := s.Find(".absolute").Text()
+		if strings.Contains(d, year+"년") && strings.Contains(d, month+"월") && strings.Contains(d, date+"일") {
+			enterLoop = true
+			// link extract
+			link, exists := s.Find(".subject").Find("a").Attr("href")
+			if exists {
+				if strings.HasPrefix(link, "http://") {
+					log.Println(link)
+				}
+			}
+			// user extract
+			user, exists := s.Find(".name span").Find("a").Attr("data-id")
+			if exists {
+				userWriteCount(user)
+				log.Println(user)
+				// user name extract
+				log.Println(s.Find(".name span").Find("a").Text())
+			}
+			return true
+		} else {
+			if len(d) <= 11 {
+				return true
+			} else {
+				if enterLoop == false {
+					return true
+				}
+				nextPage = false
+				return false
 			}
 		}
 	})
+	if nextPage {
+		getPostList(idx + 1)
+	}
 }
 
-func getPost(id string) bool {
-	return false
+func userWriteCount(user string) {
+	_, err := redisClient.Do("HINCRBY", year+month+date+"_writeCount", user, 1)
+	if err != nil {
+		log.Panic(err)
+	}
 }
 
 func main() {
@@ -90,15 +122,19 @@ func main() {
 	flag.StringVar(&month, "m", "", "target month")
 	flag.StringVar(&date, "d", "", "target date")
 	flag.Parse()
-
-	// try login
-	color.Yellow("[!] Login")
-	if login() {
-		// if Success
-		color.Green("[!] Login : PASS")
-		getPostList(1)
+	// check args
+	if username == "" || password == "" || year == "" || month == "" || date == "" {
+		color.Red("[!] Argument missing")
 	} else {
-		// if Fail
-		color.Red("[!] Login : FAIL")
+		// try login
+		color.Yellow("[!] Login")
+		if login() {
+			// if Success
+			color.Green("[!] Login : PASS")
+			getPostList(1)
+		} else {
+			// if Fail
+			color.Red("[!] Login : FAIL")
+		}
 	}
 }
